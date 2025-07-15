26cfec71641b9b413c75eb0a7e44efdbbca81a8a
Cinca Alex <gheymaru@gmail.com>
2025-07-15 20:37:19 +0300

    update

---
+++ SmartBar/SmartBar.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace SmartBar
+{
+    partial class SmartBar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchBar = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // searchBar
+            // 
+            this.searchBar.Location = new System.Drawing.Point(303, 12);
+            this.searchBar.Name = "searchBar";
+            this.searchBar.Size = new System.Drawing.Size(485, 22);
+            this.searchBar.TabIndex = 0;
+            this.searchBar.TextChanged += new System.EventHandler(this.searchBar_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(285, 84);
+            this.listBox1.TabIndex = 1;
+            // 
+            // SmartBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 98);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.searchBar);
+            this.Name = "SmartBar";
+            this.Text = "SmartBar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchBar;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ SmartBar/SmartBar.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartBar
+{
+    public partial class SmartBar : Form
+    {
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private const int HOTKEY_ID = 1;
+        private const uint MOD_ALT = 0x0001;  // Alt key
+        private const uint VK_S = 0x53;       // 'S' key
+
+
+        public SmartBar()
+        {
+            InitializeComponent();
+            RegisterHotKey(this.Handle, HOTKEY_ID, MOD_ALT, VK_S);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+
+            if (m.Msg == WM_HOTKEY && m.WParam.ToInt32() == HOTKEY_ID)
+            {
+                Point newLocation = new Point(MousePosition.X / 2, MousePosition.Y + 15);
+                this.Location = newLocation;
+            }
+
+            base.WndProc(ref m);
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, HOTKEY_ID);
+            base.OnFormClosed(e);
+        }
+
+        public static void SearchIndexedFiles(string keyword, ListBox list)
+        {
+            list.Items.Clear();
+            string connectionString = "Provider=Search.CollatorDSO;Extended Properties='Application=Windows'";
+
+            string safeKeyword = EscapeLikePattern(keyword.Replace("'", "''"));
+            string pattern = $"%{safeKeyword}%";
+            string query = $"SELECT System.ItemPathDisplay FROM SYSTEMINDEX " +
+                           $"WHERE System.ItemNameDisplay LIKE '{pattern}'";
+
+            try
+            {
+                using (OleDbConnection connection = new OleDbConnection(connectionString))
+                {
+                    connection.Open();
+                    using (OleDbCommand command = new OleDbCommand(query, connection))
+                    using (OleDbDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string path = reader.GetString(0);
+                            list.Items.Add(path);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Search failed: " + ex.Message);
+            }
+        }
+
+        private static string EscapeLikePattern(string input)
+        {
+            return input.Replace("[", "[[]")
+                        .Replace("%", "[%]")
+                        .Replace("_", "[_]")
+                        .Replace("]", "[]]");
+        }
+
+        private void searchBar_TextChanged(object sender, EventArgs e)
+        {
+            string text = searchBar.Text;
+            bool isAnApp = false;
+            bool isMath = false;
+            bool isFile = false;
+
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                listBox1.Items.Clear();
+                return;
+            }
+
+            if (text.Contains("+") || text.Contains("-") ||
+                text.Contains("*") || text.Contains("/"))
+            {
+                isMath = true;
+            }
+            else
+            {
+                string userStartMenuPath = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                    @"Microsoft\Windows\Start Menu\Programs");
+
+                foreach (var file in new DirectoryInfo(userStartMenuPath).GetFiles())
+                {
+                    if (file.Name.Contains(text))
+                    {
+                        isAnApp = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!isAnApp && !isMath)
+            {
+                SearchIndexedFiles(text, listBox1);
+                isFile = listBox1.Items.Count > 0;
+            }
+
+            if (!isFile && !isMath && !isAnApp)
+            {
+                // Your logic here
+            }
+        }
+
+
+    }
+}
